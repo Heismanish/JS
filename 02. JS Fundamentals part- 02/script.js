370bc331991138959921d7d647850e273aacc9d1
Manish Kumar Gupta <manishgu231@gmail.com>
2022-11-06 09:49:26 +0530

    Assignments 1 done

---
+++ 02. JS Fundamentals part- 02/script.js
@@ -0,0 +1,15 @@
+'use strict'; //1. shows visible errors in certain situations
+              //2. Restricts us from doing few things.
+
+let hasDriversLicense = false;
+const passTest = true;
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can derive :D');
+
+//const interface = 'Audio';
+//const private = 534;
+
+
+
+
+
